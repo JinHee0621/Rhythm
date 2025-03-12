5610e7a34043a6d42827dbbd64cf11f82a9db25c
JinHee <lsd0017@naver.com>
2025-03-12 12:57:49 +0900

    Fix Music List
    250312

--- Assets/Script/MusicListManager.cs
+++ Assets/Script/MusicListManager.cs
@@ -6,7 +6,6 @@ public class MusicListManager : MonoBehaviour
 {
     private Dictionary<int, string> musicList = new Dictionary<int, string>();
     public int current_music = 0;
-
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Script/Object/ListMusic.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListMusic : MonoBehaviour
+{
+
+    public string music_name;
+    public int difficulty;
+    public float accuracy;
+    public int music_score;
+
+    public Sprite music_image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
