658d559adce61ede4242b6930d66c35834d34336
JinHee <lsd0017@naver.com>
2025-03-17 11:10:05 +0900

    Fix Path

---
+++ Assets/Script/Object/MusicElement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicElement : MonoBehaviour
+{
+    public string music_name;
+    public int difficulty;
+    public float accuracy;
+    public int music_score;
+
+    public Sprite music_image;
+
+    public void MusicInit(string name, int diff, float acc, int score, string image_name)
+    {
+        this.music_name = name;
+        this.difficulty = diff;
+        this.accuracy = acc;
+        this.music_score = score;
+
+        
+    }
+}
