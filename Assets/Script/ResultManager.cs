afdcb83049298aecb20092b85000ed66b5666fdf
JinHee <lsd0017@naver.com>
2025-05-16 12:48:13 +0900

    Fix Game Result Set
    250516

--- Assets/Script/ResultManager.cs
+++ Assets/Script/ResultManager.cs
@@ -7,15 +7,12 @@ using DG.Tweening;
 public class ResultManager : MonoBehaviour
 {
     public GameObject resultSet;
+    public GameObject gameSet;
+    public GameObject gameGearSet;
     public Image fadeOutScreen;
 
     public int[] hitTypeCnt = new int[5];
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Text[] hitTypeCntTxt = new Text[5];
 
     //0:perfect, 1:good, 2:soso, 3:bad, 4:miss
     public void CheckHitTypeCount(int type)
@@ -38,6 +35,17 @@ public class ResultManager : MonoBehaviour
 
     IEnumerator FadeIn()
     {
+        yield return new WaitForSeconds(5f);
+
+        gameSet.SetActive(false);
+        gameGearSet.SetActive(false);
+        resultSet.SetActive(true);
+
+        for(int idx = 0; idx < 5; idx++)
+        {
+            hitTypeCntTxt[idx].text = hitTypeCnt[idx].ToString();
+        }
+
         fadeOutScreen.DOColor(new Color(0f, 0f, 0f, 0f), 1f);
         yield return new WaitForSeconds(1f);
     }
