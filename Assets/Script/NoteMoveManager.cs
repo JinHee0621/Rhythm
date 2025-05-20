7f0163db596602ce16a892458058944b4473c5e4
JinHee <lsd0017@naver.com>
2025-05-20 16:43:18 +0900

    Add Pause Game
    250520

--- Assets/Script/NoteMoveManager.cs
+++ Assets/Script/NoteMoveManager.cs
@@ -8,6 +8,7 @@ public class NoteMoveManager : MonoBehaviour
     private float yMove;
     private float default_pos;
 
+    public bool perse = false;
     public bool running;
     // Start is called before the first frame update
     void Awake()
@@ -19,12 +20,23 @@ public class NoteMoveManager : MonoBehaviour
     }
     // Update is called once per frame
     void Update()
-    {   
+    {
         if(running)
         {
             RunningGame();
-        }   
+        }
+
+        //T Key : Pause Game
+        if(Input.GetKeyDown(KeyCode.T) && running == true)
+        {
+            running = false;
+        }
+        else if(Input.GetKeyDown(KeyCode.T) && running == false)
+        {
+            StartCoroutine(Wait5Sceond());
+        }
     }
+
 
     public void RunningGame()
     {
@@ -35,4 +47,15 @@ public class NoteMoveManager : MonoBehaviour
     {
         return default_pos;
     }
+
+    IEnumerator Wait5Sceond()
+    {
+        int waitTime = 0;
+        for(waitTime = 0; waitTime < 5; waitTime++)
+        {
+            yield return new WaitForSecondsRealtime(1f);
+        }
+        running = true;
+    }
+
 }
